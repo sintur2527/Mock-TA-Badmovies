c002998f7e0e3dd62cd708ffbc05e8116815584a
Soumith Inturi <sintur2527@gmail.com>
2019-06-27 16:36:33 -0400

    second commit

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 // import AnyComponent from './components/filename.jsx'
-import Search from './components/Search.jsx'
-import Movies from './components/Movies.jsx'
+import Search from './components/Search.jsx';
+import Movies from './components/Movies.jsx';
 
 class App extends React.Component {
   constructor(props) {
-  	super(props)
-  	this.state = {
-      movies: [{deway: "movies"}],
-      favorites: [{deway: "favorites"}],
+    super(props);
+    this.state = {
+      movies: [{ deway: 'movies' }],
+      favorites: [{ deway: 'favorites' }],
       showFaves: false,
     };
-    
+
     // you might have to do something important here!
   }
 
@@ -30,20 +30,30 @@ class App extends React.Component {
   }
 
   swapFavorites() {
-  //dont touch
+    //dont touch
     this.setState({
-      showFaves: !this.state.showFaves
+      showFaves: !this.state.showFaves,
     });
   }
 
-  render () {
-  	return (
+  render() {
+    return (
       <div className="app">
-        <header className="navbar"><h1>Bad Movies</h1></header> 
-        
+        <header className="navbar">
+          <h1>Bad Movies</h1>
+        </header>
+
         <div className="main">
-          <Search swapFavorites={this.swapFavorites} showFaves={this.state.showFaves}/>
-          <Movies movies={this.state.showFaves ? this.state.favorites : this.state.movies} showFaves={this.state.showFaves}/>
+          <Search
+            swapFavorites={this.swapFavorites}
+            showFaves={this.state.showFaves}
+          />
+          <Movies
+            movies={
+              this.state.showFaves ? this.state.favorites : this.state.movies
+            }
+            showFaves={this.state.showFaves}
+          />
         </div>
       </div>
     );
